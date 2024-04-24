60f3af563587463b74c00686b0ef876db5fb6f42
laylaghill <166079094+laylaghill@users.noreply.github.com>
2024-04-23 19:20:19 -0700

    lab 4 pt. 1

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Pseudocoding: Rock, Paper, Scissors
+// Author: Layla Hill <lghill@ucsc.edu>
+// Date: 24 April 2024
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Display welcome message to players
+  //Possibly just a title block of text above game
+//Prompt Player 1 to choose rock, paper, or scissors.
+  //Button option:
+    //Create individual buttons with the text "rock,""paper,""scissors"
+    //Each button can only have one option
+  //Typed option:
+    //Allows player to type out "rock,""paper,""scissors" in a text box rather than click a button
+    //Only one option can be typed
+//Prompt Player 2 to choose rock, paper, or scissors.
+  //Same options as Player 1
+//Compare the choices made by Player 1 and Player 2.
+  //Both players must take their turns before this step is completed
+//Determine the winner based on the following rules:
+  //Rock beats scissors
+  //Scissors beats paper
+  //Paper beats rock
+//If both players choose the same option, it's a tie.
+  //Rock and rock
+  //Scissors and scissors
+  //Paper and paper
+//Display the result of the game (winner or tie).
+  //This screen should also include "play again" or "exit" as choices for the players
+  //Players must both click "play again" to start another round
+//Ask if the players want to play again.
+  //If yes, repeat steps 2-7.
+//If no, display a goodbye message and end the game.
