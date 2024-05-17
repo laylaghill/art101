91ad49d270e6222335f63b38f9db49b69b8da725
laylaghill <166079094+laylaghill@users.noreply.github.com>
2024-05-17 15:37:41 -0700

    lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,23 @@
+// index.js - Lab 11: Javascript Events and Forms
+// Author: Layla Hill
+// Date: 20 May 2024
+
+// sorts the characters of a string in alphabetical order
+function sortString(inputString) {
+    //convert string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+}
+
+// click listener for button
+$("#submit").click(function() {
+
+// get value of input field
+const userName = $("#user-name").val();
+
+// sort it
+userNameSorted = sortString(userName);
+
+// append a new div to output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
+});
